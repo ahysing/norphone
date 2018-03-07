d160cd8ba83d94ad40939d3ab8e90982b43e641b
Andreas Dreyer Hysing <andreashysing@gmail.com>
2018-03-07 19:02:34 +0100

    fixed norphone 0.1

--- src/github.com/ahysing/norphone.go
+++ src/github.com/ahysing/norphone.go
@@ -7,6 +7,10 @@ func isVowel(ch rune) bool {
 }
 
 func Norphone(word string) string {
+	if len(word) <= 1 {
+		return ""
+	}
+
 	wordNormalized := strings.ToUpper(word)
 	r := []rune(wordNormalized)
 	outputLength := len(r) * 2
@@ -16,52 +20,135 @@ func Norphone(word string) string {
 	previous := ' '
 	output := make([]rune, 0, outputLength)
 	isFirst := false
-	it := 0
-	for i := 0; i < len(r); i = i + 1 {
-		ch := r[i]
-		var currentCharacter = r[i]
+
+	for i := 0; i < len(r); i++ {
 		isLast := i == len(r)-1
 		isSecondLast := i == len(r)-2
-		if !isLast && ch != 'A' {
+		ch := r[i]
+		currentCharacter := r[i]
+		var oneAhead rune
+		if !isLast {
+			oneAhead = r[i+1]
+		}
+
+		if !isLast && ch == oneAhead && ch != 'A' {
 			ch = ' '
 		} else if isVowel(ch) && !isFirst {
 			ch = ' '
-		} else {
-			if !isLast {
-				switch ch {
-				case 'A':
-					if next == 'A' {
-						ch = 'Å'
-					}
+		} else if !isLast {
+			switch ch {
+			case 'A':
+				if oneAhead == 'A' {
+					ch = 'Å'
+				}
 
-				case 'C':
-					if next == 'H' next == 'K' {
-						ch = 'Å'
-					}
-					
-				case 'D':
-					if 'T' == next isSecondLast {
-						ch = 'T'
-						it++;
-					}
-				default:
+			case 'C':
+				if oneAhead == 'H' || oneAhead == 'K' {
+					ch = 'K'
+					i++
+				} else {
+					ch = 'K'
 				}
-			} else {
-				switch ch {
-				case 'C':
-				default:
+
+			case 'D':
+				if 'T' == oneAhead || isSecondLast {
+					ch = 'T'
+					i++
+				} else if isVowel(previous) && isLast {
+					ch = ' '
 				}
+
+			case 'G':
+				if 'H' == oneAhead {
+					i++ // 'H' is silent
+				} else if 'J' == oneAhead || 'I' == oneAhead && isFirst {
+					ch = 'J'
+					i++
+				}
+
+			case 'H':
+				if 'J' == oneAhead {
+					ch = 'J'
+					i++
+				} else if 'L' == oneAhead {
+					ch = 'L'
+					i++
+				} else if 'G' == oneAhead {
+					ch = 'G'
+					i++
+				} else if 'R' == oneAhead {
+					ch = 'R'
+					i++
+				}
+
+			case 'K':
+				if 'I' == oneAhead || 'J' == oneAhead {
+					ch = 'X'
+					i++
+				}
+
+			case 'L':
+				if 'D' == oneAhead && isSecondLast {
+					ch = 'L'
+					i++
+				}
+
+			case 'N':
+				if 'D' == oneAhead {
+					i++
+				}
+
+			case 'P':
+				if 'H' == oneAhead {
+					ch = 'F'
+					i++ // eat the 'H'
+				}
+
+			case 'T':
+				if 'H' == oneAhead {
+					i++ // 'H' is silent
+				}
+
+			case 'W':
+				ch = 'V'
+
+			case 'X':
+				output[pos] = 'K'
+				pos++
+				ch = 'S'
+
+			case 'Z':
+				ch = 'S'
+			}
+		} else {
+			switch ch {
+			case 'D':
+				if isVowel(previous) && isLast {
+					ch = ' '
+				}
+
+			case 'W':
+				ch = 'V'
+
+			case 'X':
+				output[pos] = 'K'
+				pos++
+				ch = 'S'
+
+			case 'Z':
+				ch = 'S'
 			}
 		}
-		
+
 		if ch != ' ' {
 			output[pos] = ch
-			pos ++
+			pos++
 		}
-		
+
 		previous = currentCharacter
 		isFirst = false
 	}
 
-	return string(output)
+	var outputFinal = output[:pos]
+	return string(outputFinal)
 }
